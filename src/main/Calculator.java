f5ac79bb1ae7aeab34b7863fe6976862d31c9fb3
Aaron Juarez <aarjuamtz@gmail.com>
2017-06-08 13:42:55 -0500

    Test: Enter and Drop values

--- src/main/Calculator.java
+++ src/main/Calculator.java
@@ -5,11 +5,19 @@ import java.math.BigDecimal;
 public class Calculator {
 	private BigDecimal accumulator = BigDecimal.ZERO;
 
-    public BigDecimal getAccumulator() {
-        return accumulator;
-    }
+	public BigDecimal getAccumulator() {
+		return accumulator;
+	}
 
-    public void setAccumulator(BigDecimal accumulator) {
-        this.accumulator = accumulator;
-    }
+	public void setAccumulator(BigDecimal accumulator) {
+		this.accumulator = accumulator;
+	}
+
+	public void enter() {
+
+	}
+
+	public void drop() {
+
+	}
 }
